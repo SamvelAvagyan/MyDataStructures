77f5d62c8166596df6a68152ea40853a8445737e
SamvelAvagyan <avagyansamvel07@gmail.com>
2023-06-13 00:10:07 +0400

    Create Node and LinkedList classes, add AddFirst AddLast functionalities for LinkedList

---
+++ MyDataStructures/MyDataStructures/LinkedList.cs
@@ -0,0 +1,49 @@
+﻿namespace MyDataStructures
+{
+    class LinkedList<T>
+    {
+        private Node<T> _head;
+        private Node<T> _tail;
+        public int Count { get; private set; }
+
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+            Count = 0;
+        }
+
+        public LinkedList(T data)
+        {
+            _head = new Node<T>(data);
+            _tail = _head;
+            Count = 1;
+        }
+
+        public void AddFirst(T data)
+        {
+            var node = new Node<T>(data);
+            node.Next = _head;
+            _head = node;
+            Count++;
+        }
+
+        public void AddLast(T data)
+        {
+            var node = new Node<T>(data);
+
+            if(_head == null)
+            {
+                _head = node;
+                _tail = _head;
+                Count = 1;
+            }
+            else
+            {
+                _tail.Next = node;
+                _tail = _tail.Next;
+                Count++;
+            }
+        }
+    }
+}
--- MyDataStructures/MyDataStructures/Program.cs
+++ MyDataStructures/MyDataStructures/Program.cs
@@ -6,7 +6,12 @@ namespace MyDataStructures
     {
         static void Main(string[] args)
         {
-
+            System.Collections.Generic.LinkedList<int> linkedList;
+            LinkedList<int> list = new LinkedList<int>();
+            list.AddLast(10);
+            list.AddLast(11);
+            list.AddLast(12);
+            list.AddFirst(5);
         }
     }
 }
